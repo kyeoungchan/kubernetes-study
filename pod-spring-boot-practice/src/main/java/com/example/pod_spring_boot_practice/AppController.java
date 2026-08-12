7904e478ff7186710f025f5e195a4f1e3d831aa3
wkc1002 <wkc1002@naver.com>
2026-08-13 06:42:50 +0900

    init

---
+++ pod-spring-boot-practice/src/main/java/com/example/pod_spring_boot_practice/AppController.java
@@ -0,0 +1,12 @@
+package com.example.pod_spring_boot_practice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello, World!";
+    }
+}
